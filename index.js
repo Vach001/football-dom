29a7dfa2f55581c59aee60e8c9da6b0dc0cf6fb0
Vache Apinyan <apinyan.v@gmail.com>
2023-05-03 15:23:02 +0400

    fix: remove comments

--- index.js
+++ index.js
@@ -3,24 +3,6 @@ import aplMatches from "./src/assets/data/matches/apl.json" assert { type: "json
 import { matchResultOption } from "./src/constants/matches.constants.js";
 import getClabsStat from "./src/services/matches.services.js";
 import { calcClabStat } from "./src/helpers/calcStats.helpers.js";
-// console.log(aplClubs);
-// console.log(aplMatches);
-
-/*
-{
-    "Team Name": {
-        mp: 0,
-        w: 0,
-        d: 0,
-        l: 0,
-        gf: 0,
-        ga: 0,
-        gd: 0,
-        pts: 0,
-    }
-}
-*/
-
 
 
 console.log(getClabsStat(aplMatches.matches, matchResultOption));
