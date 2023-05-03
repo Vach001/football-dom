319f8e761e2b382151ba84de3d9dd314a20f09c5
Vache Apinyan <apinyan.v@gmail.com>
2023-05-03 17:04:28 +0400

    feat: add createElement generic function

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import aplClubs from "./src/assets/data/leagues/apl.json" assert { type: "json" };
-import aplMatches from "./src/assets/data/matches/apl.json" assert { type: "json" };
-import { matchResultOption } from "./src/constants/matches.constants.js";
-import getClabsStat from "./src/services/matches.services.js";
-import { calcClabStat } from "./src/helpers/calcStats.helpers.js";
+import Table from "./src/components/Table/Table.js";
 
+const root = document.getElementById("root")
 
-console.log(getClabsStat(aplMatches.matches, matchResultOption));
+root.append(Table())
