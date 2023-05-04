f25e131c8a6506fa5a43dace6642e6f00edc3f9b
Vache Apinyan <apinyan.v@gmail.com>
2023-05-04 23:29:46 +0400

    feat: add linerContents and change header

--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -2,18 +2,19 @@ import { createElement } from "../../helpers/createElement.js";
 import TableHeaderInfo from "./TableHeaderInfo.js";
 
 export function TableHeader() {
-    const TableHeaderImage = createElement("img", {
-        src: "./src/assets/images/leagues/premier-league.png",
-        alt: "Premier League",
-        cass: "header__logo"
-    })
+  const TableHeaderImage = createElement("img", {
+    src: "./src/assets/images/leagues/premier-league.png",
+    alt: "Premier League",
+    class: "header__logo",
+  });
 
   return createElement(
-    "header", {},
+    "header",
+    {},
     TableHeaderImage,
     TableHeaderInfo({
-    title: "Premier League",
-    description: "Season 2022 - 2023",
-  })
+      title: "Premier League",
+      description: "Season 2022 - 2023",
+    })
   );
 }
--- src/helpers/createElement.js
+++ src/helpers/createElement.js
@@ -1,12 +1,12 @@
 export function createElement(tagName, props = {}, ...childrens) {
-    const tag = document.createElement(tagName)
+  const tag = document.createElement(tagName);
 
-    Object.keys(props).forEach((prop) => {
-        tag.setAttribute(prop, props[prop])
-    });
-    childrens.forEach((child) => {
-        tag.append(child);
-    });
-    
-    return tag;
+  Object.keys(props).forEach((prop) => {
+    tag.setAttribute(prop, props[prop]);
+  });
+  childrens.forEach((child) => {
+    tag.append(child);
+  });
+
+  return tag;
 }
